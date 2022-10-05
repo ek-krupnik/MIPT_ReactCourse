8dba2f2e506b108a88c9d397c063af946a435168
krupnik ekaterina <krupnik.ev@phystech.edu>
2022-10-06 00:38:49 +0300

    mvp with infinite likes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import {Test} from './components/test'
+import data from './assets/mock_data.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    const [counter, setCounter] = useState(0)
+
+    const inc = () => {
+        setCounter (counter + 1)
+    }
+
+    return (
+        <div className="App">
+            <div className="common">
+                {data.map(item => <Test value={item}/>)}
+            </div>
+        </div>
   );
 }
 
