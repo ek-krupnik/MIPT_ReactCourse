16b77db7496859198d2a00fe3ff3e369dc01c1af
krupnik ekaterina <krupnik.ev@phystech.edu>
2022-10-06 18:57:25 +0300

    done

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -14,7 +14,9 @@ export function Test(props) {
     const [markerLiked, setMarkerLiked] = useState(false)
     const [markerDisliked, setMarkerDisliked] = useState(false)
 
-    const likes_color = markerLiked ? "rgb(196, 242, 181)" : (markerDisliked ? "rgb(242, 181, 183)" : "rgb(255, 235, 205)")
+    const likes_color = markerLiked ? 
+                        "rgb(196, 242, 181)" : 
+                        (markerDisliked ? "rgb(242, 181, 183)" : "rgb(255, 235, 205)")
 
     const like = () => {
         if (markerLiked) {
@@ -28,27 +30,15 @@ export function Test(props) {
     }
 
     const dislike = () => {
-        var chimg = document.getElementsByClassName(s.dislike);
         if (markerDisliked) {
             setCounter (counter + 1)
             setMarkerDisliked (false)
-            chimg.style.backgroundImage = "url('../images/dislike_den.png')";
         }
         if (!markerDisliked & !markerLiked) {
             setCounter (counter - 1)
             setMarkerDisliked (true)
-            chimg.style.backgroundImage = "url('../images/dislike_set.png')";
-            console.log(chimg.style.backgroundImage)
         }
     }
-
-    // useEffect(() => {
-    //     setCounter(JSON.parse(window.localStorage.getItem('counter')));
-    // }, []);
-    
-    // useEffect(() => {
-    //     window.localStorage.setItem('counter', counter);
-    // }, [counter]);
 
     return (
         <>
@@ -62,25 +52,20 @@ export function Test(props) {
                 </div>
 
                 <div className={s.image}>
-                    <img
-                        src={image_url}
-                        alt="Animal"
-                    />
-                    
+                    <img src={image_url} alt="Animal"/>
                 </div>
             </div>
+
             <div className={s.likes_construction}>
 
 
-                <div className={s.dislike} onClick={dislike}>
-                </div>
+                <div className={s.dislike} onClick={dislike}></div>
 
                 <div className={s.counter} style={{backgroundColor: likes_color}}>
                     {counter}
                 </div>
 
-                <div className={s.like} onClick={like}>
-                </div>
+                <div className={s.like} onClick={like}></div>
             </div>
         </>
     )
